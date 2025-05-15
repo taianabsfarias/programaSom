86c3d7aa1c6c091ee348456c8e731e28889c92eb
Taiana Farias <taianafariasbs@gmail.com>
2025-05-15 10:49:09 -0300

    commit

--- graf/windows/mainwin/uiMainWin.py
+++ graf/windows/mainwin/uiMainWin.py
@@ -11,3 +11,18 @@ class Ui_MainWindow(object):
         parent.setMinimumSize(960, 540)
 
         self.central_frame = QFrame()
+        self.central_frame.setStyleSheet("background-color: #D4E2F0")
+
+
+        self.main_layout = QHBoxLayout(self.central_frame)
+
+        self.left_menu = QFrame()
+        self.left_menu.setStyleSheet("background-color: #C8CCD1")
+
+        self.content = QFrame()
+        self.content.setStyleSheet("background-color: #C8CCD1")
+
+        self.main_layout.addWidget(self.left_menu)
+        self.main_layout.addWidget(self.content)
+
+        parent.setCentralWidget(self.central_frame)
